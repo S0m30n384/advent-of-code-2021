67dcc3c096235c3e586849bbdb8346a1d288dd01
S0m30n384 <40548794+s0m30n384@users.noreply.github.com>
2021-12-01 11:05:52 +0100

    Day 1.

--- src/Day01.kt
+++ src/Day01.kt
@@ -6,7 +6,7 @@ private const val PART2_SLIDING_WINDOW_SIZE = 3
 
 fun main() {
 
-    val testInput = readInput("Day01_test")
+    val testInput = readInput("Day01_test").map { it.toInt() }
 
     val part1testInputResult = part1(testInput)
     println(part1testInputResult)
@@ -16,18 +16,17 @@ fun main() {
     println(part2testInputResult)
     check(part2testInputResult == PART2_CRITERIA) { "The result does not match the example in the description!" }
 
-    val input = readInput("Day01")
+    val input = readInput("Day01").map { it.toInt() }
     println(part1(input))
     println(part2(input))
 }
 
-fun part1(input: List<String>): Int {
-    val intList = input.map { it.toInt() }
+fun part1(input: List<Int>): Int {
     var depthIncreaseCounter = 0
-    var previousMeasurement = intList.first()
+    var previousMeasurement = input.first()
 
-    for (index in 1..intList.lastIndex) {
-        val currentMeasurement = intList[index]
+    for (index in 1..input.lastIndex) {
+        val currentMeasurement = input[index]
         if (currentMeasurement > previousMeasurement) depthIncreaseCounter++
         previousMeasurement = currentMeasurement
     }
@@ -35,16 +34,17 @@ fun part1(input: List<String>): Int {
     return depthIncreaseCounter
 }
 
-fun part2(input: List<String>): Int {
-    val intList = input.map { it.toInt() }
+fun part2(input: List<Int>): Int {
     var depthIncreaseCounter = 0
-    var previousMeasurement = intList.subList(0, PART2_SLIDING_WINDOW_SIZE).sum()
+    var previousMeasurement = measureDepth(input, 0, PART2_SLIDING_WINDOW_SIZE)
 
-    for (i in 1..intList.size - PART2_SLIDING_WINDOW_SIZE) {
-        val currentMeasurement = intList.subList(i, i + PART2_SLIDING_WINDOW_SIZE).sum()
+    for (i in 1..input.size - PART2_SLIDING_WINDOW_SIZE) {
+        val currentMeasurement = measureDepth(input, i, i + PART2_SLIDING_WINDOW_SIZE)
         if (currentMeasurement > previousMeasurement) depthIncreaseCounter++
         previousMeasurement = currentMeasurement
     }
 
     return depthIncreaseCounter
 }
+
+fun measureDepth(input: List<Int>, startIndex: Int, endIndex: Int) = input.subList(startIndex, endIndex).sum()
