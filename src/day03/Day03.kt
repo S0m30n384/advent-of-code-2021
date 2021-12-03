d4eee45e61f48cc4a5ce25e897c02f3077fa0ec0
S0m30n384 <40548794+s0m30n384@users.noreply.github.com>
2021-12-03 21:43:57 +0100

    Day 3 part 2.

--- src/day03/Day03.kt
+++ src/day03/Day03.kt
@@ -4,30 +4,12 @@ import readInputToStringList
 
 private const val PART1_CRITERIA = 198
 
-/**
- * [Description](https://adventofcode.com/2021/day/3).
- */
-fun main() {
+private const val PART2_CRITERIA = 230
 
-    val testInput = readInputToStringList("day03/Day03_test")
-
-    val part1testInputResult = part1(testInput)
-    check(part1testInputResult == PART1_CRITERIA) { "The result does not match the example in the description! Criteria: $PART1_CRITERIA. Result: $part1testInputResult." }
-
-//    val part2testInputResult = part2(testInput)
-//    check(part2testInputResult == PART2_CRITERIA) { "The result does not match the example in the description! Criteria: $PART2_CRITERIA. Result: $part2testInputResult." }
-
-    val input = readInputToStringList("day03/Day03")
-    println(part1(input))
-//    println(part2(input))
-}
-
-fun part1(input: List<String>): Int {
-
-    val maxLengthOfInput = input.maxOf { it.length }
+fun createSumList(input: List<String>, stringMaxLength: Int): MutableList<Int> {
     val sumList = mutableListOf<Int>()
 
-    for (i in 0 until maxLengthOfInput) {
+    for (i in 0 until stringMaxLength) {
         sumList.add(i, 0)
     }
 
@@ -38,6 +20,31 @@ fun part1(input: List<String>): Int {
             }
         }
     }
+    return sumList
+}
+
+/**
+ * [Description](https://adventofcode.com/2021/day/3).
+ */
+fun main() {
+
+    val testInput = readInputToStringList("day03/Day03_test")
+
+    val part1testInputResult = part1(testInput)
+    check(part1testInputResult == PART1_CRITERIA) { "The result does not match the example in the description! Criteria: $PART1_CRITERIA. Result: $part1testInputResult." }
+
+    val part2testInputResult = part2(testInput)
+    check(part2testInputResult == PART2_CRITERIA) { "The result does not match the example in the description! Criteria: $PART2_CRITERIA. Result: $part2testInputResult." }
+
+    val input = readInputToStringList("day03/Day03")
+    println(part1(input))
+    println(part2(input))
+}
+
+fun part1(input: List<String>): Int {
+
+    val maxLengthOfInput = input.maxOf { it.length }
+    val sumList = createSumList(input, maxLengthOfInput)
 
     val charArray = CharArray(maxLengthOfInput)
 
@@ -48,6 +55,29 @@ fun part1(input: List<String>): Int {
 
     return gammaRate * epsilonRate
 
+}
+
+fun part2(input: List<String>): Int {
+    val oxygenGeneratorRating = calculateOxygenGeneratorRating(input, 0)
+    val co2ScrubberRating = calculateCO2ScrubberRating(input, 0)
+
+    return oxygenGeneratorRating.toInt(2) * co2ScrubberRating.toInt(2)
+}
+
+fun calculateOxygenGeneratorRating(input: List<String>, i: Int): String {
+    val currentCharToKeep = if (createSumList(input, input.maxOf { it.length })[i] >= (input.size / 2 + input.size % 2)) '1' else '0'
+    val filter = input.filter { it[i] == currentCharToKeep }
+    return if (filter.size > 1) {
+        calculateOxygenGeneratorRating(filter, i + 1)
+    } else filter.first()
+}
+
+fun calculateCO2ScrubberRating(input: List<String>, i: Int): String {
+    val currentCharToKeep = if (createSumList(input, input.maxOf { it.length })[i] < (input.size / 2 + input.size % 2)) '1' else '0'
+    val filter = input.filter { it[i] == currentCharToKeep }
+    return if (filter.size > 1) {
+        calculateCO2ScrubberRating(filter, i + 1)
+    } else filter.first()
 }
 
 fun inverse(bitString: String) = bitString.replace("1", "x").replace("0", "1").replace("x", "0")
