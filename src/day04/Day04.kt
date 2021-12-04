5f5a2a83105fa3e98123083dd5e3b8fe373ed696
S0m30n384 <40548794+s0m30n384@users.noreply.github.com>
2021-12-04 21:16:29 +0100

    Day 4 part 2 with bugfix in `boardWon()` method.

--- src/day04/Day04.kt
+++ src/day04/Day04.kt
@@ -4,7 +4,9 @@ import readInputToStringList
 
 private const val PART1_CRITERIA = 4512
 
-data class BingoBoard(val board: MutableList<MutableList<BingoCell>> = mutableListOf())
+private const val PART2_CRITERIA = 1924
+
+data class BingoBoard(var boardWon: Boolean = false, val board: MutableList<MutableList<BingoCell>> = mutableListOf())
 
 data class BingoCell(val number: Int, var isMarked: Boolean)
 
@@ -38,20 +40,26 @@ fun main() {
     val part1testInputResult = part1(testInput.first, testInput.second)
     check(part1testInputResult == PART1_CRITERIA) { "The result does not match the example in the description! Criteria: $PART1_CRITERIA. Result: $part1testInputResult." }
 
-//    val part2testInputResult = part2(testInput)
-//    check(part2testInputResult == PART2_CRITERIA) { "The result does not match the example in the description! Criteria: $PART2_CRITERIA. Result: $part2testInputResult." }
-//
+    val part2testInputResult = part2(testInput.first, testInput.second)
+    check(part2testInputResult == PART2_CRITERIA) { "The result does not match the example in the description! Criteria: $PART2_CRITERIA. Result: $part2testInputResult." }
+
     val input = readBingoInput(readInputToStringList("day04/Day04"))
     println(part1(input.first, input.second))
-//    println(part2(input))
+    println(part2(input.first, input.second))
 }
 
 fun boardWon(bingoBoard: BingoBoard): Boolean {
     bingoBoard.board.forEach { mutableList ->
-        if (mutableList.all { it.isMarked }) return true
+        if (mutableList.all { it.isMarked }) {
+            bingoBoard.boardWon = true
+            return true
+        }
     }
-    for (i in 0..5) {
-        if (bingoBoard.board.all { it[0].isMarked }) return true
+    for (i in 0 until 5) {
+        if (bingoBoard.board.all { it[i].isMarked }) {
+            bingoBoard.boardWon = true
+            return true
+        }
     }
     return false
 }
@@ -63,7 +71,20 @@ fun part1(input: MutableList<BingoBoard>, chosenNumberList: List<Int>): Int {
                 row.firstOrNull { it.number == chosenNumber }?.isMarked = true
             }
             if (boardWon(bingoBoard)) {
-                println(chosenNumber)
+                return bingoBoard.board.sumOf { row -> row.sumOf { if (!it.isMarked) it.number else 0 } } * chosenNumber
+            }
+        }
+    }
+    return 0
+}
+
+fun part2(input: MutableList<BingoBoard>, chosenNumberList: List<Int>): Int {
+    chosenNumberList.forEach { chosenNumber ->
+        input.forEach { bingoBoard ->
+            bingoBoard.board.forEach { row ->
+                row.firstOrNull { it.number == chosenNumber }?.isMarked = true
+            }
+            if (boardWon(bingoBoard) && input.none { !it.boardWon }) {
                 return bingoBoard.board.sumOf { row -> row.sumOf { if (!it.isMarked) it.number else 0 } } * chosenNumber
             }
         }
